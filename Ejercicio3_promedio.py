a537f190b15171f6217df790e219ed35d535b9f7
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-01-22 12:45:40 -0500

    Ejercicio3_Simplificado

--- Ejercicio3_promedio.py
+++ Ejercicio3_promedio.py
@@ -1,11 +1,11 @@
 #Escriba un programa que calcule el promedio de 4 notas ingresadas por el usuario:
 
-from statistics import mean
-
-notas_list = []
+suma = 0
 
 for i in range(4):
     num = float(input(f"Ingresa el número {i+1}: "))
-    notas_list.append(num)
-    
-print(f"El promedio de las notas es {mean(notas_list)}")
+    suma += num
+
+promedio = suma/4
+
+print(f"El promedio de las notas es {promedio}")
